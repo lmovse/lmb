7e2ac66aa0e4eb5c2781805bf4422421a4661d79
zhangguangguang <lmovsezhang@163.com>
2017-11-29 23:11:57 +0800

    so?

---
+++ src/main/java/info/lmovse/blog/core/pojo/dto/CommentContent.java
@@ -0,0 +1,21 @@
+package info.lmovse.blog.core.pojo.dto;
+
+import info.lmovse.blog.core.pojo.po.Content;
+
+/**
+ * Created by lmovse on 2017/8/25.
+ * Tomorrow is a nice day.
+ */
+public class CommentContent {
+
+    private Content content;
+
+    public Content getContent() {
+        return content;
+    }
+
+    public void setContent(Content content) {
+        this.content = content;
+    }
+
+}
